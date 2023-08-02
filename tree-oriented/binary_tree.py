1ca0c709e64b1463dc3f4b5688a8e219a8b60644
whingss <quickchop0@gmail.com>
2023-08-02 18:50:45 +0200

    init binary search tree

---
+++ tree-oriented/binary_tree.py
@@ -0,0 +1,20 @@
+from tree_node import TreeNode
+
+class BinarySearchTree:
+    def __init__(self, root) -> None:
+        self.root = root
+    
+    def add_node(self, node) -> None or TreeNode:
+        pass
+
+    def remove_node(self, node) -> None or TreeNode:
+        pass
+
+    def find_value(self, current: TreeNode, value) -> None or type:
+        pass
+
+    def find_value_iterative(self, current: TreeNode, value) -> None or type:
+        pass
+
+    def find_tree_node(self, value) -> None or TreeNode:
+        pass
