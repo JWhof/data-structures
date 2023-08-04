c1cb52fa70faf4b4a3b5a129d91b4a3b05c2757d
whingss <quickchop0@gmail.com>
2023-08-04 10:57:43 +0200

    add node with two children removal functionality, comments, fixes

--- tree-oriented/binary_tree.py
+++ tree-oriented/binary_tree.py
@@ -32,6 +32,7 @@ class BinarySearchTree:
     
 
     def remove_node(self, node) -> None or TreeNode:
+        "Removes a node from the tree. NOTE: to pass in a value to remove, use the self.find_node(value) function as a parameter."
         # Cases: removing a leaf node, removing an internal node with one child, and removing an internal node with two children
         if self.root == None or node == None: # Check that tree is not empty
             return None
@@ -43,34 +44,47 @@ class BinarySearchTree:
                 node.parent.left = None
             else:
                 node.parent.right = None
-            return
+            return node
 
         if node.left == None or node.right == None: # Single child internal node case
             child = node.left
-            
+
             if child == None:
                 child = node.right
 
             child.parent = node.parent
             
             if child.parent == None:
-                child = self.root
+                self.root = child
             elif node.value < node.parent.value:
                 node.parent.left = child
             else:
                 node.parent.right = child
-            return
+            return node
         
-        #TODO: add internal node with two children case
+        successor = node.right # Two children internal node case
+        while successor.left != None:
+            successor = successor.left
+        self.remove_node(successor)
 
-
+        if node.parent == None:
+            self.root = successor 
+        elif node.parent.left == node:
+            node.parent.left = successor
+        else:
+            node.parent.right = successor
         
+        successor.parent = node.parent
+        successor.left = node.left
         
-
-
+        successor.right = node.right
+        if node.right != None:
+            node.right.parent = successor
+        
+        return node
 
     def find_value(self, current: TreeNode, value: int) -> None or TreeNode:
-        "Finds a given value using recursion."
+        "Finds a given value using recursion. NOTE: pass in self.root for the current parameter (first)."
         if current == None:
             return None
         if value < current.value and current.left != None:
@@ -86,17 +100,73 @@ class BinarySearchTree:
         while current != None and value != current.value:
             if value < current.value:
                 current = current.left
-            if value > current.value:
+            elif value > current.value:
                 current = current.right
         return current
 
 my_binary_search_tree = BinarySearchTree()
 
-node_one = TreeNode(1)
+node_one = TreeNode(3)
 
 my_binary_search_tree.insert_tree_node(node_one)
 
-node_two = TreeNode(2)
+node_two = TreeNode(7)
+my_binary_search_tree.insert_tree_node(node_two)
 
-print(my_binary_search_tree.find_value(my_binary_search_tree.root, 1))
-print(my_binary_search_tree.find_value_iterative(my_binary_search_tree.root, 1))
+
+print(my_binary_search_tree.find_value(my_binary_search_tree.root, 3))
+print(my_binary_search_tree.find_value_iterative(my_binary_search_tree.root, 3))
+
+node_three = TreeNode(4)
+my_binary_search_tree.insert_tree_node(node_three)
+
+node_four = TreeNode(11)
+my_binary_search_tree.insert_tree_node(node_four)
+
+print(f"Node with value 7 left child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 7).left.value}") # Should print 4
+print(f"Node with value 7 right child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 7).right.value}") # Should print 11
+
+# The binary tree looks like this:
+#               3
+#                \
+#                 7
+#                / \
+#               4  11
+# We now try to remove node 7:
+
+my_binary_search_tree.remove_node(my_binary_search_tree.find_value(my_binary_search_tree.root, 7))
+
+print(f"Node with value 11 left child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 11).left.value}") # Should print 4
+print(f"Node with value 11 right child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 11).right}") # Should print None
+
+
+# The binary tree now looks like this:
+#               3
+#                \
+#                11
+#                /
+#               4
+# We now try to remove node 11:
+
+my_binary_search_tree.remove_node(my_binary_search_tree.find_value(my_binary_search_tree.root, 11))
+print(f"Node with value 4 left child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 4).left}") # Should print None
+print(f"Node with value 4 right child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 4).right}") # Should print None
+
+# The binary tree now looks like this:
+#               3
+#                \
+#                 4
+# We now try to remove node 3:
+
+my_binary_search_tree.remove_node(my_binary_search_tree.find_value(my_binary_search_tree.root, 3))
+print(f"Node with value 4 left child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 4).left}") # Should print None
+print(f"Node with value 4 right child: {my_binary_search_tree.find_value(my_binary_search_tree.root, 4).right}") # Should print None
+
+# The binary tree now looks like this:
+#               4
+# We now try to remove node 4:
+
+my_binary_search_tree.remove_node(my_binary_search_tree.find_value(my_binary_search_tree.root, 4))
+print(f"Binary tree root value: {my_binary_search_tree.root}")
+
+# And we're done! :)
